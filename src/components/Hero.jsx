3c0c69a21c204c902bb7aa6d73e0a983df949de4
preetiiik <preetikaroli10@example.com>
2026-07-27 11:19:08 +0530

    Improve home hero responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,42 +1,39 @@
 import "../styles/HeroHome.css";
 import { Link } from "react-router-dom";
 
-function HeroH() {
+function Hero() {
   return (
-    <section className="hero" data-aos="fade-in">
-      <div className="hero-content">
+    <section className="home-hero" data-aos="fade-in">
+      <div className="home-hero-content">
         {/* Badge */}
-        <div className="hero-badge">
-          <span className="badge-dot"></span>
+        <div className="home-hero-badge">
+          <span className="home-badge-dot"></span>
           <span>GUNTUR'S FINEST SINCE 1998</span>
         </div>
 
         {/* Heading */}
-        <h1 className="hero-title">
-            Premium Red
-            <br />
-            <span>Chillies,</span> Straight
-            <br />
-            from the Source
+        <h1 className="home-hero-title">
+          Premium Red
+          <br />
+          <span>Chillies,</span> Straight
+          <br />
+          from the Source
         </h1>
 
         {/* Paragraph */}
-        <p className="hero-description">
+        <p className="home-hero-description">
           Delivering handpicked, export-grade chillies, chilli powder,
           and flakes with rich color, strong aroma, and authentic heat
           from India's spice capital.
         </p>
 
         {/* Buttons */}
-        <div className="hero-buttons">
-          {/* <button className="primary-btn">
+        <div className="home-hero-buttons">
+          <Link to="/Products" className="home-primary-btn">
             Explore Products
-          </button> */}
-          <Link to="/Products" className="primary-btn">
-            Explore Products
-            </Link>
+          </Link>
 
-          <button className="secondary-btn">
+          <button className="home-secondary-btn">
             Watch Our Story
           </button>
         </div>
@@ -45,4 +42,4 @@ function HeroH() {
   );
 }
 
-export default HeroH;
+export default Hero;
